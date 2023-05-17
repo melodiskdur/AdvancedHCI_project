7f54b17981c2c30c9c4dbd628db814c0ad4b63a8
antjah191 <antjah191@studentmail.oru.se>
2023-05-18 00:03:31 +0200

    tests on implementing pyvista with/into pyqt5. Works well makes it much easier to dynamically change parameters in the visualization

--- other/visualization_experiments/pyvista_test.py
+++ other/visualization_experiments/pyvista_test.py
@@ -1,87 +1,57 @@
 import numpy as np
 import pyvista as pv
 from sklearn.neighbors import NearestNeighbors
-import threading
-from pynput import keyboard
-
-
-################################################
-#////////////////--- Globals ---/////////////////
-
-# TODO: remove globals if possible
-default_view = 'xy'
-reset_view = False
-
-
-#####################################################################
-#////////////////--- Hotkeys and their functions ---/////////////////
-
-# Define all the hotkeys
-HOTKEYS = {'<ctrl>+r': lambda : reset_camera_view('<ctrl>+r'),
-            '<ctrl>+z': lambda : function_1('<ctrl>+z'),
-            '<ctrl>+x': lambda : function_2('<ctrl>+x')}
-
-def reset_camera_view(hotkey):
-    global default_view
-    global reset_view
-    #print('Executed reset_camera_view:',hotkey)
-    default_view = 'xy'
-    reset_view = True
-
-def function_1(hotkey):
-    #print('Executed function_1:',hotkey)
-    pass
-
-def function_2(hotkey):
-    #print('Executed function_2:',hotkey)
-    pass
-
+import cmocean
 
 #################################################
 #////////////////--- Helpers ---/////////////////
 
-def normalize_point(p):
-    return p / np.sqrt(np.sum(p**2))
-
-def normalize_points(points):
-    return np.array([normalize_point(p) for p in points])
-
-def set_z_value(p,val):
-    return np.array([p[0],p[1],val])
-
-def set_z_values(points,val):
-    return np.array([set_z_value(p,val) for p in points])
-
-def calculate_scalars(points, num_neighbors):
+def create_nn_scalars(points:np.ndarray, num_neighbors, scalar_threshold=0.5):
+    """
+    Calculate the nearest neighbor scalars for all points.
+    A higher values are give to points that have neighbors that are close to it.
+    Input:
+        points: a np.ndarray of 3d points
+        num_neighbors: the amount of neighbors that is considered for each point
+        scalar_threshold: points assigned a scalar value below the threshold are given a scalar value of 0
+    Output:
+        scalars: a np.ndarray of float scalar values for each 3d point
+    """
+    if num_neighbors <= 0:
+        num_neighbors = 1
+    
     # Compute the nearest neighbors for each point
     neighbors = NearestNeighbors(n_neighbors=num_neighbors).fit(points)
     distances, _ = neighbors.kneighbors(points)
     
     # Calculate the scalars for the points based on the distances
     # Add a small value to prevent division by zero
-    scalars = 1.0 / (distances.mean(axis=1) + 1e-6)
-    
-    return scalars
+    scalars = (1.0 / (distances.mean(axis=1) + 1e-10))
 
-def create_points_and_scalars(num_points_per_frame:int=100, 
-                              frame_width:int=10, 
-                              frame_height:int=8, 
-                              num_frames:int=10, 
-                              dist_between_frames:int=1,
-                              num_neighbors:int=6):
+    # Scale the scalars to fit fully within [0,1]
+    max_s = max(list(scalars))
+    scalars = [s / max_s for s in scalars]
+
+    # Set all the scalars that are below the threshold to 0
+    scalars = [(0 if s < scalar_threshold else s) for s in scalars]
+
+    return np.array(scalars)
+
+def create_3d_points(
+        num_points_per_frame:int=100, 
+        frame_dimensions:tuple[int,int]=(10,8), 
+        num_frames:int=10, 
+        dist_between_frames:int=1,):
     """
     Create all the points and their corresponding scalars
     Input:
         num_points_per_frame: the number of points in each frame
-        frame_width: the width of the frame
-        frame_height: the height of the frame
+        frame_dimensions: a tuple with the width and height of the
         num_frames: the number of frames
         dist_between_frames: the distance between each frame in the plot
     Output:
         points: a np.array with 3d points
-        scalars: a np.array with float scalars for each 3d point in points
     """
-
     # Initialize the points list
     points = []
 
@@ -89,8 +59,8 @@ def create_points_and_scalars(num_points_per_frame:int=100,
     for i in range(num_frames):
 
         # Calculate some random xy values withing the width and height of the frame
-        x_values = np.random.uniform(low=0.0, high=frame_width, size=num_points_per_frame)
-        y_values = np.random.uniform(low=0.0, high=frame_height, size=num_points_per_frame)
+        x_values = np.random.uniform(low=0.0, high=frame_dimensions[0], size=num_points_per_frame)
+        y_values = np.random.uniform(low=0.0, high=frame_dimensions[1], size=num_points_per_frame)
 
         # Create points and add a specific z-value for each frame
         frame_points = np.array([np.array([x,y,-i*dist_between_frames]) for x,y in zip(x_values,y_values)])
@@ -102,144 +72,150 @@ def create_points_and_scalars(num_points_per_frame:int=100,
     # Change points to a numpy array for ease of use
     points = np.array(points)
 
-    # Create all the scalars for each point
-    scalars = np.array(calculate_scalars(points, num_neighbors=num_neighbors))
-
-    return points, scalars
-
-
-############################################
-#////////////////--- IO ---/////////////////
-
-def change_plotter_parameters(pl:pv.Plotter):
-    global default_view
-    global reset_view
-
-    # If we want to reset the view ot the default
-    if reset_view:
-        pl.camera_position = default_view
-        reset_view = False
-
-
-###################################################
-#////////////////--- Threading ---/////////////////
-
-def activate_thread(target:object, args:tuple):
-    # Create a thread
-    thread = threading.Thread(target=target, args=args)
-    
-    # Start the thread
-    thread.start()
-
-    return thread
-
-def deactivate_thread(thread):
-    # Kill the thread
-    thread.join()
-
+    return points
 
 ###################################################
 #////////////////--- Rendering ---/////////////////
 
-def plot_data(plotter:pv.Plotter, points:list, scalars:list, width:int=10, height:int=8, num_frames:int=10, dist_between_frames:int=1):
-
-    # NOTE: this cubemesh can be removed. Primarily for debugging
-    # Add the cube mesh to encapsulate all the data/frames
-    plotter.add_mesh(
+def add_plotter_cube(plotter:pv.Plotter,
+                     cube_dims:tuple[int,int,int],
+                     color:str='white',
+                     style:str='wireframe',
+                     line_width:int=2):
+    """
+    Basically just the plotter.add_mesh() method, but with
+    an easier to use interface were only the needed 
+    parameters are inclueded
+    """
+    actor = plotter.add_mesh(
         pv.Cube(
-            center=(width/2,height/2,-(num_frames*dist_between_frames)/2),
-            x_length=width,
-            y_length=height,
-            z_length=num_frames*dist_between_frames), 
-        color='white', 
-        style='wireframe', 
-        line_width=2)
+            center=(cube_dims[0]/2,cube_dims[1]/2,-cube_dims[2]/2),
+            x_length=cube_dims[0],
+            y_length=cube_dims[1],
+            z_length=cube_dims[2]), 
+        color=color, 
+        style=style, 
+        line_width=line_width)
+    
+    return actor
 
-    # Add all the points with their scalars to the plot
-    plotter.add_points(
+def add_plotter_points(plotter:pv.Plotter,
+                       points,
+                       scalars,
+                       render_points_as_spheres:bool=True,
+                       style:str='points_gaussian',
+                       cmap=cmocean.cm.thermal,
+                       emissive:bool=False,
+                       rgba:bool=False,
+                       point_size:int=10):
+    """
+    Basically just the plotter.add_points() method, but with
+    an easier to use interface were only the needed 
+    parameters are inclueded
+    """
+    actor = plotter.add_points(
         points,
-        render_points_as_spheres=True,
-        style='points_gaussian',
-        emissive=True,
+        render_points_as_spheres=render_points_as_spheres,
+        style=style,
+        cmap=cmap,
+        emissive=emissive,
         scalars=scalars,
-        rgba=False,
-        point_size=10,
+        rgba=rgba,
+        point_size=point_size,
         show_scalar_bar=True,
         scalar_bar_args= {'label_font_size':10,'outline':False})
+    
+    return actor
 
-    # Add some text at the top
-    plotter.add_text('pyvista_test.py',font_size=6, color='w')
 
+def set_plotter_parameters(
+        plotter:pv.Plotter,
+        bg_color:str='k',
+        cam_pos:str='xy',
+        cam_zoom=1.0,
+        cam_rotation:tuple[int,int,int]=(45,15,0),
+        add_axes:bool=True,
+        reset_cam_orientation:bool=False
+        ): 
+    """
+    Sets some stuff. Need to be improved.
+    """
     # Set a background color
-    plotter.background_color = 'k'
+    plotter.background_color = bg_color
 
-    # Set the default camera view position and orientation
-    plotter.camera_position = default_view
+    # Add some text to the plot
+    plotter.add_text('AdvancedHCI_project',font_size=6, color='w')
+    
+    if reset_cam_orientation:
 
-    # Add the xyz-axes visible in the lower left
-    plotter.add_axes()
+        # Set the default camera view position and orientation
+        plotter.camera_position = cam_pos 
+
+        # Set a default initial camera rotation
+        plotter.camera.azimuth = cam_rotation[0]
+        plotter.camera.elevation = cam_rotation[1]
+        plotter.camera.roll = cam_rotation[2]
 
     # Set an intial camera zoom 
-    plotter.camera.zoom(1.2)
+    plotter.camera.zoom(cam_zoom)
 
-    # Show the plot i.e. everything we have added to the plotter
-    # NOTE: interactive_update is set to tru to allow plotter.update() to run
-    # This way we enable handling of input fro the user when plotting
-    plotter.show(interactive_update=True)
-    
-    # Continously update the window
-    while True:
-
-        # Change plot parameters depending on the users input
-        change_plotter_parameters(plotter)
-
-        # Update the window
-        plotter.update()
+    # Add the xyz-axes visible in the lower left
+    if add_axes:
+        plotter.add_axes()
 
 
 ##############################################
 #////////////////--- Main ---/////////////////
 
-if __name__ == "__main__": 
+def test_main():
 
     #-------------------
     # Plotting parameters
     num_points_per_frame = 20
-    frame_width = 10
-    frame_height = 8
-    num_frames = 30
-    dist_between_frames = 0.5
+    frame_dimensions = (1024,768) 
+    num_frames = 50
+    dist_between_frames = 50
+    num_neighbors = 4
+    scalar_threshold = 0.5
+    #-------------------
 
     #-------------------
-    # Create all the points and their corresponding scalars
-    points, scalars = create_points_and_scalars(num_points_per_frame,frame_width,frame_height,num_frames,dist_between_frames)
+    # Create all the points
+    points = create_3d_points(
+        num_points_per_frame=num_points_per_frame,
+        frame_dimensions=frame_dimensions,
+        num_frames=num_frames,
+        dist_between_frames=dist_between_frames)
+    
+    # Create all the scalars for each point
+    scalars = create_nn_scalars(
+        points, 
+        num_neighbors=num_neighbors,
+        scalar_threshold=scalar_threshold)
 
     #-------------------
     # Create a pyvista plotting object
-    num_rows, num_columns = 1, 1
-    plotter = pv.Plotter(shape=(num_rows, num_columns))
-
-    # Set current subplot
+    plotter = pv.Plotter(shape=(1, 1))
     plotter.subplot(0, 0)
 
-    #-------------------
-    # Create and start a thread for plotting
-    plot_thread = activate_thread(target=plot_data, args=(plotter,points,scalars,frame_width,frame_height,num_frames,dist_between_frames))
-    
-    #-------------------
-    # Create and start a thread for handling input
-    with keyboard.GlobalHotKeys(HOTKEYS) as input_thread:
-        
-        # NOTE: this line below can substitute the creation of plot_thread
-        #plot_data(plotter,100,10,8,10)
+    # Add the points to the plot
+    add_plotter_points(plotter,points,scalars)
 
-        # Kill the input thread
-        deactivate_thread(input_thread)
+    # Add a cube around the points to the plot
+    add_plotter_cube(plotter,(frame_dimensions[0],frame_dimensions[1],num_frames*dist_between_frames))
     
-    #-------------------
-    #Kill the plotting thread
-    deactivate_thread(plot_thread)
+    # NOTE: set parameters after adding everything to the plotter
+    set_plotter_parameters(plotter,reset_cam_orientation=True)
+    
+    # Display everything added to the plot
+    plotter.show()
 
+if __name__ == "__main__": 
+    test_main()
+
+    
+
+    
     
 
 
